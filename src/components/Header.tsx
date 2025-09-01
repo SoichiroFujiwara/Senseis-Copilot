6698c1e19747c6852097ada75c7d974b987cf72d
SoichiroFujiwara <soichirofuji0319@keio.jp>
2025-09-01 21:36:32 +0900

    added title to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,11 @@
 export default function Header() {
     return (
-        <div>
-            <h1 className="bg-red-100 text-2xl font-bold">Header</h1>
+        <div className="bg-purple-100 p-4">
+            <div className="flex items-end gap-2">
+                <div className="text-2xl font-bold">Sensei&apos;s Copilot</div>
+                <div className="text-sm text-gray-500">v0.1.0</div>
+            </div>
+            
         </div>
     );
 }
