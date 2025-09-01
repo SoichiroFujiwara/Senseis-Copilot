c575dd30635803dfcb171baf1562bdfadcc4212b
SoichiroFujiwara <soichirofuji0319@keio.jp>
2025-09-01 21:39:32 +0900

    added Link to title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className="bg-purple-100 p-4">
-            <div className="flex items-end gap-2">
+            <Link href="/" className="flex items-end gap-2">
                 <div className="text-2xl font-bold">Sensei&apos;s Copilot</div>
                 <div className="text-sm text-gray-500">v0.1.0</div>
-            </div>
-            
+            </Link>
         </div>
     );
 }
